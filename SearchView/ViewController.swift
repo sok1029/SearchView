b01b735b52a7bf27b849910ad8194d8f73f343dd
석진영 <sok10291@naver.com>
2019-07-28 12:54:36 +0900

    *. run 시 indicator 추가

--- SearchView/ViewController.swift
+++ SearchView/ViewController.swift
@@ -11,9 +11,16 @@ import RxSwift
 import RxCocoa
 import RealmSwift
 
-
 class ViewController: UIViewController {
     let disposeBag = DisposeBag()
+    let activityIndicator: UIActivityIndicatorView = {
+        let indicator = UIActivityIndicatorView.init()
+        indicator.style = .whiteLarge
+        indicator.color = .blue
+        indicator.hidesWhenStopped = true
+        return indicator
+    }()
+
     @IBOutlet weak var searchView: SearchView!
     
     override func viewDidLoad() {
@@ -22,9 +29,15 @@ class ViewController: UIViewController {
         //        let textFieldView = SearchTextFieldView.init(frame: CGRect(x: 50, y: 50, width: 250, height: 100))
         //        self.view.addSubview(textFieldView)
         
-        searchView.actWhenRun = {
+        activityIndicator.center = self.view.center
+        self.view.addSubview(activityIndicator)
+      
+        searchView.actWhenRun = {[weak self] in
             //input Your act when running
-            print("doSomethingInViewController")
+            self?.activityIndicator.startAnimating()
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {[weak self] in
+                self?.activityIndicator.stopAnimating()
+            }
         }
     }
 }
